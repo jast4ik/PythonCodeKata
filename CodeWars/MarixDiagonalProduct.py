6b3dec89a3e6bc6570e743395b1bf08f8d4cf53c
Yevgen Iliashchienko <yevgen.iliashchienko@gmail.com>
2021-08-27 22:19:48 +0300

    Bugfix.

--- CodeWars/MarixDiagonalProduct.py
+++ CodeWars/MarixDiagonalProduct.py
@@ -5,25 +5,25 @@ https://www.codewars.com/kata/590bb735517888ae6b000012/train/python
 import math
 
 
+def rotate_matrix_90(m: list, times=1):
+    rotated = m.copy()
+    for i in range(times):
+        rotated = list(zip(*rotated))[::-1]
+    return rotated
+
+
+def get_diag_sum(m: list):
+    diag = [[m[i][i + j] for i in range(len(m) - j)] for j in range(1, len(m))]
+    diag += [[m[i + j][i] for i in range(len(m) - j)] for j in range(len(m))]
+    return sum([math.prod(c_diag) for c_diag in diag])
+
+
 def sum_prod_diags(matrix):
+    sum1 = get_diag_sum(matrix)
+    matrix = rotate_matrix_90(matrix, 3)
+    sum2 = get_diag_sum(matrix)
 
-    # sum1 = [matrix[i][i + j] for j in range(len(matrix) - 1) for i in range(len(matrix) - j)]
-    # sum1 = [matrix[i + j][j] for i in range(1, len(matrix) - 1) for j in range(len(matrix) - i)]
-    # sum1 = [matrix[i][i + 3] for i in range(len(matrix) - 3)]
-    sum1 = list()
-    sum2 = list()
-
-    for index in range(len(matrix) - 1):
-        sum1.append([matrix[i][i + index] for i in range(len(matrix) - index)])
-    for index in range(1, len(matrix) - 1):
-        sum1.append([matrix[i + index][i] for i in range(len(matrix) - index)])
-
-    sum2 = [matrix[i][i - 1] for i in range(1, len(matrix))]
-
-    for i in reversed(range(1, len(matrix))):
-        print(i)
-    print(sum2)
-    return [[]]
+    return sum1 - sum2
 
 
 if __name__ == "__main__":
