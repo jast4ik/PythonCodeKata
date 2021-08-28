c9a99c82e4a81a2f5426047b4bda407387815dd7
USER_ <yevgen.iliashchienko@gmail.com>
2021-08-28 12:22:44 +0300

    Bugfix.

--- CodeWars/duplicates_everywhere.py
+++ CodeWars/duplicates_everywhere.py
@@ -6,14 +6,10 @@ https://www.codewars.com/kata/5e8dd197c122f6001a8637ca
 def remove_duplicate_ids(input_dict: dict):
     s_list = {x: [y for y in input_dict[x]] for x in sorted(input_dict, key=int)}
 
-    for key in s_list:
-        for i in reversed(range(len(s_list[key]))):
-            if s_list[key].count(s_list[key][i]) > 1:
-                s_list[key].pop(i)
+    uniques = set()
 
-    for max_key in reversed(s_list):
-        for key in s_list:
-            s_list[key] = [x for x in s_list[key] if x not in s_list[max_key]] if key != max_key else s_list[key]
+    for key in reversed(s_list):
+        s_list[key] = [uniques.add(x) or x for x in s_list[key] if x not in uniques]
 
     return s_list
 
